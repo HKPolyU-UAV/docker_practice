4cdee164f5422ff396e779abe2a6ec41fe63e6e2
pattylo <19451945as@gmail.com>
2023-09-03 15:57:38 +0800

    commit a bit again

--- run_hehe.sh
+++ run_hehe.sh
@@ -4,14 +4,34 @@ echo "RUN YOUR OWN IMAGE"
 
 image=$1
 
+echo "GOT GPU? y/n:"
+read got_gpu
+
+gpu_enabled="--gpus all"
+if [ "$got_gpu" == "y" ] || [ "$got_gpu" == "Y" ]; then
+    echo ""
+else
+    gpu_enabled=""
+fi
+
+if [ "$(docker images -q $image 2> /dev/null)" == "" ]; then
+    echo ""
+    echo "ERROR. PLEASE CHECK THE EXISTENCE OF UR IMAGE!"
+    echo ""
+    exit 1
+else
+    echo ""
+    echo "NOW RUNNING IMAGE -> CONTAINER"
+    echo ""
+fi
+
+echo "NOW RUNNING IMAGE -> CONTAINER"
+echo "CONTAINER BASED ON IMAGE: $image"
+
 XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 touch $XAUTH
 xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
-echo "NOW RUNNING IMAGE -> CONTAINER"
-echo "IMAGE BASED ON: $image"
-
 
 sudo docker run \
   -it \
