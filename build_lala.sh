5f4f0171232b3912befb3b59f729ecdcdff11287
RockyJBL <jiangbailun@gmail.com>
2023-09-05 16:15:18 +0800

    commit

--- build_lala.sh
+++ build_lala.sh
@@ -12,19 +12,8 @@ else
     distro="${distro}-raw"
 fi
 
-echo "WITH AIRO PACKAGES? y/n:"
-read airo_pkg_or_no
-
-if [ "$airo_pkg_or_no" == "y" ] || [ "$airo_pkg_or_no" == "Y" ]; then
-    distro="${distro}-pkg"
-    final_name="${final_name}-pkg"
-else
-    distro="${distro}-nopkg"
-    final_name="${final_name}-nopkg"
-fi
-
 echo "BUILDING $final_name DOCKER IMAGE."
 
 docker build \
-    -f dkerfiles/Dockerfile.$distro \
+    -f dkerfiles/Dockerfile.vim4$distro \
     -t airo_noetic_lala:$final_name .
